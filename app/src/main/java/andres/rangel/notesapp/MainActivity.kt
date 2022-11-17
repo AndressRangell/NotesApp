008d2760ec5d9cd2c032acb3f64d2cda434f37b6
AndresRangelMeli <andres.rangel@mercadolibre.com.co>
2022-11-17 07:03:41 -0500

    Initial commit

---
+++ app/src/main/java/andres/rangel/notesapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package andres.rangel.notesapp
+
+import andres.rangel.notesapp.ui.theme.NotesAppTheme
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NotesAppTheme {
+
+            }
+        }
+    }
+}
